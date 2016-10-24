74834890129282936270eeb4a54c7d6d036ff425
Thomas Ongeri <ongerit@gmail.com>
2016-10-24 01:20:08 -0400

    versioned files

---
+++ Gruntfile.js
@@ -0,0 +1,390 @@
+// Generated on 2016-10-22 using
+// generator-webapp 0.5.1
+'use strict';
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// If you want to recursively match all subfolders, use:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {
+
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
+
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+  // Configurable paths
+  var config = {
+    app: 'app',
+    findhome: 'findhome'
+  };
+
+  // Define the configuration for all the tasks
+  grunt.initConfig({
+
+    // Project settings
+    config: config,
+
+    // Watches files for changes and runs tasks based on the changed files
+    watch: {
+      bower: {
+        files: ['bower.json'],
+        tasks: ['wiredep']
+      },
+      js: {
+        files: ['<%= config.app %>/scripts/{,*/}*.js'],
+        tasks: ['jshint'],
+        options: {
+          livereload: true
+        }
+      },
+      jstest: {
+        files: ['test/spec/{,*/}*.js'],
+        tasks: ['test:watch']
+      },
+      gruntfile: {
+        files: ['Gruntfile.js']
+      },
+      styles: {
+        files: ['<%= config.app %>/styles/{,*/}*.css'],
+        tasks: ['newer:copy:styles', 'autoprefixer']
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          '<%= config.app %>/{,*/}*.html',
+          '.tmp/styles/{,*/}*.css',
+          '<%= config.app %>/images/{,*/}*'
+        ]
+      }
+    },
+
+    // The actual grunt server settings
+    connect: {
+      options: {
+        port: 9000,
+        open: true,
+        livereload: 35729,
+        // Change this to '0.0.0.0' to access the server from outside
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          middleware: function(connect) {
+            return [
+              connect.static('.tmp'),
+              connect().use('/bower_components', connect.static('./bower_components')),
+              connect.static(config.app)
+            ];
+          }
+        }
+      },
+      test: {
+        options: {
+          open: false,
+          port: 9001,
+          middleware: function(connect) {
+            return [
+              connect.static('.tmp'),
+              connect.static('test'),
+              connect().use('/bower_components', connect.static('./bower_components')),
+              connect.static(config.app)
+            ];
+          }
+        }
+      },
+      findhome: {
+        options: {
+          base: '<%= config.findhome %>',
+          livereload: false
+        }
+      }
+    },
+
+    // Empties folders to start fresh
+    clean: {
+      findhome: {
+        files: [{
+          dot: true,
+          src: [
+            '.tmp',
+            '<%= config.findhome %>/*',
+            '!<%= config.findhome %>/.git*'
+          ]
+        }]
+      },
+      server: '.tmp'
+    },
+
+    // Make sure code styles are up to par and there are no obvious mistakes
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      all: [
+        'Gruntfile.js',
+        '<%= config.app %>/scripts/{,*/}*.js',
+        '!<%= config.app %>/scripts/vendor/*',
+        'test/spec/{,*/}*.js'
+      ]
+    },
+
+    // Mocha testing framework configuration options
+    mocha: {
+      all: {
+        options: {
+          run: true,
+          urls: ['http://<%= connect.test.options.hostname %>:<%= connect.test.options.port %>/index.html']
+        }
+      }
+    },
+
+    // Add vendor prefixed styles
+    autoprefixer: {
+      options: {
+        browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1']
+      },
+      findhome: {
+        files: [{
+          expand: true,
+          cwd: '.tmp/styles/',
+          src: '{,*/}*.css',
+          dest: '.tmp/styles/'
+        }]
+      }
+    },
+
+    // Automatically inject Bower components into the HTML file
+    wiredep: {
+      app: {
+        ignorePath: /^\/|\.\.\//,
+        src: ['<%= config.app %>/index.html'],
+        exclude: ['bower_components/bootstrap/findhome/js/bootstrap.js']
+      }
+    },
+
+    // Renames files for browser caching purposes
+    rev: {
+      findhome: {
+        files: {
+          src: [
+            '<%= config.findhome %>/scripts/{,*/}*.js',
+            '<%= config.findhome %>/styles/{,*/}*.css',
+            '<%= config.findhome %>/images/{,*/}*.*',
+            '<%= config.findhome %>/styles/fonts/{,*/}*.*',
+            '<%= config.findhome %>/*.{ico,png}'
+          ]
+        }
+      }
+    },
+
+    // Reads HTML for usemin blocks to enable smart builds that automatically
+    // concat, minify and revision files. Creates configurations in memory so
+    // additional tasks can operate on them
+    useminPrepare: {
+      options: {
+        dest: '<%= config.findhome %>'
+      },
+      html: '<%= config.app %>/index.html'
+    },
+
+    // Performs rewrites based on rev and the useminPrepare configuration
+    usemin: {
+      options: {
+        assetsDirs: [
+          '<%= config.findhome %>',
+          '<%= config.findhome %>/images',
+          '<%= config.findhome %>/styles'
+        ]
+      },
+      html: ['<%= config.findhome %>/{,*/}*.html'],
+      css: ['<%= config.findhome %>/styles/{,*/}*.css']
+    },
+
+    // The following *-min tasks produce minified files in the findhome folder
+    imagemin: {
+      findhome: {
+        files: [{
+          expand: true,
+          cwd: '<%= config.app %>/images',
+          src: '{,*/}*.{gif,jpeg,jpg,png}',
+          dest: '<%= config.findhome %>/images'
+        }]
+      }
+    },
+
+    svgmin: {
+      findhome: {
+        files: [{
+          expand: true,
+          cwd: '<%= config.app %>/images',
+          src: '{,*/}*.svg',
+          dest: '<%= config.findhome %>/images'
+        }]
+      }
+    },
+
+    htmlmin: {
+      findhome: {
+        options: {
+          collapseBooleanAttributes: true,
+          collapseWhitespace: true,
+          conservativeCollapse: true,
+          removeAttributeQuotes: true,
+          removeCommentsFromCDATA: true,
+          removeEmptyAttributes: true,
+          removeOptionalTags: true,
+          removeRedundantAttributes: true,
+          useShortDoctype: true
+        },
+        files: [{
+          expand: true,
+          cwd: '<%= config.findhome %>',
+          src: '{,*/}*.html',
+          dest: '<%= config.findhome %>'
+        }]
+      }
+    },
+
+    // By default, your `index.html`'s <!-- Usemin block --> will take care
+    // of minification. These next options are pre-configured if you do not
+    // wish to use the Usemin blocks.
+    // cssmin: {
+    //   findhome: {
+    //     files: {
+    //       '<%= config.findhome %>/styles/main.css': [
+    //         '.tmp/styles/{,*/}*.css',
+    //         '<%= config.app %>/styles/{,*/}*.css'
+    //       ]
+    //     }
+    //   }
+    // },
+    // uglify: {
+    //   findhome: {
+    //     files: {
+    //       '<%= config.findhome %>/scripts/scripts.js': [
+    //         '<%= config.findhome %>/scripts/scripts.js'
+    //       ]
+    //     }
+    //   }
+    // },
+    // concat: {
+    //   findhome: {}
+    // },
+
+    // Copies remaining files to places other tasks can use
+    copy: {
+      findhome: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: '<%= config.app %>',
+          dest: '<%= config.findhome %>',
+          src: [
+            '*.{ico,png,txt}',
+            'images/{,*/}*.webp',
+            '{,*/}*.html',
+            'styles/fonts/{,*/}*.*'
+          ]
+        }, {
+          src: 'node_modules/apache-server-configs/findhome/.htaccess',
+          dest: '<%= config.findhome %>/.htaccess'
+        }, {
+          expand: true,
+          dot: true,
+          cwd: 'bower_components/bootstrap/findhome',
+          src: 'fonts/*',
+          dest: '<%= config.findhome %>'
+        }]
+      },
+      styles: {
+        expand: true,
+        dot: true,
+        cwd: '<%= config.app %>/styles',
+        dest: '.tmp/styles/',
+        src: '{,*/}*.css'
+      }
+    },
+
+    // Run some tasks in parallel to speed up build process
+    concurrent: {
+      server: [
+        'copy:styles'
+      ],
+      test: [
+        'copy:styles'
+      ],
+      findhome: [
+        'copy:styles',
+        'imagemin',
+        'svgmin'
+      ]
+    }
+  });
+
+
+  grunt.registerTask('serve', 'start the server and preview your app, --allow-remote for remote access', function (target) {
+    if (grunt.option('allow-remote')) {
+      grunt.config.set('connect.options.hostname', '0.0.0.0');
+    }
+    if (target === 'findhome') {
+      return grunt.task.run(['build', 'connect:findhome:keepalive']);
+    }
+
+    grunt.task.run([
+      'clean:server',
+      'wiredep',
+      'concurrent:server',
+      'autoprefixer',
+      'connect:livereload',
+      'watch'
+    ]);
+  });
+
+  grunt.registerTask('server', function (target) {
+    grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
+    grunt.task.run([target ? ('serve:' + target) : 'serve']);
+  });
+
+  grunt.registerTask('test', function (target) {
+    if (target !== 'watch') {
+      grunt.task.run([
+        'clean:server',
+        'concurrent:test',
+        'autoprefixer'
+      ]);
+    }
+
+    grunt.task.run([
+      'connect:test',
+      'mocha'
+    ]);
+  });
+
+  grunt.registerTask('build', [
+    'clean:findhome',
+    'wiredep',
+    'useminPrepare',
+    'concurrent:findhome',
+    'autoprefixer',
+    'concat',
+    'cssmin',
+    'uglify',
+    'copy:findhome',
+    'rev',
+    'usemin',
+    'htmlmin'
+  ]);
+
+  grunt.registerTask('default', [
+    'newer:jshint',
+    'test',
+    'build'
+  ]);
+};
---
+++ app/scripts/main.js
@@ -0,0 +1,127 @@
+'use strict';
+
+// Changes XML to JSON
+function xmlToJson(xml) {
+	// Create the return object
+	var obj = {};
+	if (xml.nodeType === 1) { // element
+		// do attributes
+		if (xml.attributes.length > 0) {
+		obj['@attributes'] = {};
+			for (var j = 0; j < xml.attributes.length; j++) {
+				var attribute = xml.attributes.item(j);
+				obj['@attributes'][attribute.nodeName] = attribute.nodeValue;
+			}
+		}
+	} else if (xml.nodeType === 3) { // text
+		obj = xml.nodeValue;
+	}
+
+	// do children
+	if (xml.hasChildNodes()) {
+		for(var i = 0; i < xml.childNodes.length; i++) {
+			var item = xml.childNodes.item(i);
+			var nodeName = item.nodeName;
+			if (typeof(obj[nodeName]) === 'undefined') {
+				obj[nodeName] = xmlToJson(item);
+			} else {
+				if (typeof(obj[nodeName].push) === 'undefined') {
+					var old = obj[nodeName];
+					obj[nodeName] = [];
+					obj[nodeName].push(old);
+				}
+				obj[nodeName].push(xmlToJson(item));
+			}
+		}
+	}
+	return obj;
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+function sendFormData() {
+  //stop the form from submitting normally
+  event.preventDefault();
+  //send the data using post with element values
+  var formAddress = $('.address').val(); // jshint ignore:line
+  var cityAddress = $('.city').val(); // jshint ignore:line
+  var stateAddress = $('.state').val(); // jshint ignore:line
+  var zipAddress = $('.zip').val(); // jshint ignore:line
+
+
+  var settings = {
+    'async': true,
+    'crossDomain': true,
+    'url': 'http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz19ktrnf9pmz_64j9s&address='+formAddress+'&citystatezip='+cityAddress+'%20'+stateAddress+'%20'+zipAddress+'&rentzestimate=true',
+    'method': 'POST',
+    'headers': {
+    'authorization': 'Bearer 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',
+    'content-type': 'application/json',
+    'cache-control': 'no-cache',
+    'postman-token': '8c89a29a-6f8f-676a-d823-05d8aeca3ed1'
+  },
+  'processData': false,
+};
+
+  $.ajax(settings).done(function (response) {
+  console.log();
+
+  var jsonData = xmlToJson(response);
+  var xml = response, // jshint ignore:line
+  xmlDoc = $.parseXML(response),
+  $xml = $( xmlDoc ); // jshint ignore:line
+
+  console.log(xmlDoc);
+  var result = jsonData['SearchResults:searchresults'].response.results.result;
+  var street = result.address.street['#text'];
+  var price = parseFloat(result.zestimate.amount['#text']);
+  var high = parseFloat(result.rentzestimate.valuationRange.high['#text']);
+  var low = parseFloat(result.rentzestimate.valuationRange.low['#text']);
+
+  // for google maps
+  var longitude = parseFloat(result.address.longitude['#text']); // jshint ignore:line
+  var latitude = parseFloat(result.address.latitude['#text']); // jshint ignore:line
+
+    console.log(result);
+
+  $('.output').text(JSON.stringify(street)).fadeIn();
+  $('.price').text('$' + numberWithCommas(price)).fadeIn();
+  $('.high').text('$' + numberWithCommas(high)).fadeIn();
+  $('.low').text('$' + numberWithCommas(low)).fadeIn();
+  $('h3').fadeIn();
+
+  var map;
+  function initMap(longitude,latitude,formAddress) {
+    console.log(longitude);
+    console.log(latitude);
+
+    var myLatLng = {lat: latitude, lng: longitude};
+
+    map = new google.maps.Map(document.getElementById('map'), { // jshint ignore:line
+      center: myLatLng, // jshint ignore:line
+      zoom: 16,
+      styles: [{'featureType':'administrative','elementType':'labels.text.fill','stylers':[{'color':'#444444'}]},{'featureType':'landscape','elementType':'all','stylers':[{'color':'#f2f2f2'}]},{'featureType':'poi','elementType':'all','stylers':[{'visibility':'off'}]},{'featureType':'road','elementType':'all','stylers':[{'saturation':-100},{'lightness':45}]},{'featureType':'road.highway','elementType':'all','stylers':[{'visibility':'simplified'}]},{'featureType':'road.arterial','elementType':'labels.icon','stylers':[{'visibility':'off'}]},{'featureType':'transit','elementType':'all','stylers':[{'visibility':'off'}]},{'featureType':'water','elementType':'all','stylers':[{'color':'#fdcb08'},{'visibility':'on'}]}],
+      label: formAddress
+    });
+
+  var marker = new google.maps.Marker({  // jshint ignore:line
+      position: myLatLng,  // jshint ignore:line
+      map: map,  // jshint ignore:line
+      title: formAddress  // jshint ignore:line
+    });
+  }
+  initMap(longitude,latitude,formAddress); // jshint ignore:line
+  }).fail(
+    function(res) {
+      console.log(res);
+      if(res === 'undefined') {
+        $('.error').text('Sorry we can not  find this address').fadeIn();
+        return false;
+      }
+    }
+  );
+}
+
+$('form').bind('submit', sendFormData);
